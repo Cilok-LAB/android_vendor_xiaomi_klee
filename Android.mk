2b5e9f452bbd0ca327188dd016cc16c10e23895a
kylieeXD <khayloaftck@gmail.com>
2026-06-23 16:14:14 +0000

    klee: Extract vendor using python extract utils
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,32 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),klee)
+
+$(call add-radio-file-sha1-checked,radio/apusys.img,51cabc49e940f819936d7a5400606b7603b18a2a)
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,7d56f68129768673fce76342058ba77a5eeb6ee9)
+$(call add-radio-file-sha1-checked,radio/ccu.img,4ca861097dce52c957eb9fb92018356c2c3a978e)
+$(call add-radio-file-sha1-checked,radio/connsys_bt.img,d9321c4eebc5b3a70466b90b2561a80774f48b5d)
+$(call add-radio-file-sha1-checked,radio/connsys_gnss.img,969f2868ef3ef7050e67edc7d5ba0c5997140090)
+$(call add-radio-file-sha1-checked,radio/connsys_wifi.img,40109efc48687758d0a81f3a3406ae4b013a148d)
+$(call add-radio-file-sha1-checked,radio/dpm.img,af5efc59557c399835aed97df5c669919817eef2)
+$(call add-radio-file-sha1-checked,radio/gpueb.img,f20efbce0df8e06a291a947b134598abefa7432e)
+$(call add-radio-file-sha1-checked,radio/gz.img,2d8afb226cd670d710c0e338d86a2be02c6d1b7c)
+$(call add-radio-file-sha1-checked,radio/lk.img,ef04b0c0034b82c3a82b065b317a77acf43d3271)
+$(call add-radio-file-sha1-checked,radio/logo.img,74e2f2c42a98e841fd34f38c89abf4849ff4fad0)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,eaaff18d3b25c4a58a6f1501cf5346572225bb13)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,e4a6fcc4ba6b872414490123986121ea7c1d87b8)
+$(call add-radio-file-sha1-checked,radio/modem.img,e7f4effff3c3cb127e94dfd75d437ace4c4583b3)
+$(call add-radio-file-sha1-checked,radio/mvpu_algo.img,90c9feccd8d9ac2a22654de396ff76f2dd92d4a0)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,ad48f66211d93c253abe837021f212aaa09712a7)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,846e7e8fc26bc994d00b1c2211a22df4e6ce87b2)
+$(call add-radio-file-sha1-checked,radio/scp.img,ce9c81ae65a500a68b1e3ee87810c248d5cbbe0a)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,36dfd83051d752d14f6d6cf242f54e35d2b75597)
+$(call add-radio-file-sha1-checked,radio/sspm.img,68b0496b5c81b1f0dcf0deaf4b74bfd018e46cb4)
+$(call add-radio-file-sha1-checked,radio/tee.img,d2ef3f6828536496d8035be443e4618363ee1c32)
+$(call add-radio-file-sha1-checked,radio/vcp.img,03edb544418a3bf48f7fba9e2d85899f05217138)
+
+endif
